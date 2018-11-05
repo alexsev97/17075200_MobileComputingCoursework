8698c2fcf88d7821b868e8fa57d21055892e3174
alexsev97 <alsevill@ucm.es>
2018-11-05 19:21:53 +0000

    Ground and background moving

--- Sevilla_MobileComputingCoursework/ViewController.swift
+++ Sevilla_MobileComputingCoursework/ViewController.swift
@@ -11,20 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var groundView: UIImageView!
+    @IBOutlet weak var background1View: UIImageView!
+    @IBOutlet weak var background2View: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.animate(self.groundView)
-
+        self.move(self.groundView, duration: 0.3, translation: -37)
+        
+        self.move(self.background1View, duration: 16, translation: -513)
+        
+        self.move(self.background2View, duration: 16, translation: -513)
     }
         
-        func animate(_ image: UIImageView) {
-            UIView.animate(withDuration: 0.3, delay: 0, options: .curveLinear, animations: {
-                image.transform = CGAffineTransform(translationX: -37, y: 0)
+    func move(_ image: UIImageView, duration: Double, translation: CGFloat) {
+            UIView.animate(withDuration: duration, delay: 0, options: .curveLinear, animations: {
+                image.transform = CGAffineTransform(translationX: translation, y: 0)
             }) { (success: Bool) in
                 image.transform = CGAffineTransform.identity
-                self.animate(image)
+                self.move(image, duration: duration, translation: translation)
             }
         }
 
