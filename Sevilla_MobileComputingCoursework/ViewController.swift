0861991af54a504531877f1628c8876458d9c221
alexsev97 <alsevill@ucm.es>
2018-11-05 20:25:34 +0000

    Background movement finished

--- Sevilla_MobileComputingCoursework/ViewController.swift
+++ Sevilla_MobileComputingCoursework/ViewController.swift
@@ -13,15 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var groundView: UIImageView!
     @IBOutlet weak var background1View: UIImageView!
     @IBOutlet weak var background2View: UIImageView!
+    @IBOutlet weak var background3View: UIImageView!
+    @IBOutlet weak var cloud1View: UIImageView!
+    @IBOutlet weak var cloud2View: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(background1View)
+        self.view.addSubview(background2View)
+        self.view.addSubview(background3View)
+        self.view.addSubview(groundView)
+        self.view.addSubview(cloud1View)
+        self.view.addSubview(cloud2View)
+        
+        self.move(self.background1View, duration: 15, translation: -528)
+        self.move(self.background2View, duration: 15, translation: -528)
+        self.move(self.background3View, duration: 15, translation: -528)
+        
         self.move(self.groundView, duration: 0.3, translation: -37)
         
-        self.move(self.background1View, duration: 16, translation: -513)
+         self.move(self.cloud1View, duration: 23, translation: -150)
+        self.move(self.cloud2View, duration: 23, translation: -500)
         
-        self.move(self.background2View, duration: 16, translation: -513)
     }
         
     func move(_ image: UIImageView, duration: Double, translation: CGFloat) {
