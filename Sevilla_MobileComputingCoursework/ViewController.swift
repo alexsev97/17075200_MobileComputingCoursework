85be3ed353e7f1bcf66accb206c720023b1b9791
alexsev97 <alsevill@ucm.es>
2018-11-07 15:25:01 +0000

    Gravity behaviour added to main sprite

--- Sevilla_MobileComputingCoursework/ViewController.swift
+++ Sevilla_MobileComputingCoursework/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+protocol subviewDelegate {
+    func addGravity()
+    func removeGravity()
+}
+
+class ViewController: UIViewController, subviewDelegate {
+    // Moving background items
     @IBOutlet weak var groundView: UIImageView!
     @IBOutlet weak var background1View: UIImageView!
     @IBOutlet weak var background2View: UIImageView!
@@ -17,6 +22,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var cloud1View: UIImageView!
     @IBOutlet weak var cloud2View: UIImageView!
     
+    // Main character items
+    @IBOutlet weak var marioView: DraggedImageView!
+    
+    
+    // Behavior items
+    var dynamicAnimator: UIDynamicAnimator!
+    var collisionBehavior: UICollisionBehavior!
+    var gravityBehavior: UIGravityBehavior!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +40,8 @@ class ViewController: UIViewController {
         self.view.addSubview(groundView)
         self.view.addSubview(cloud1View)
         self.view.addSubview(cloud2View)
+        self.view.addSubview(marioView)
+        marioView.myDelegate = self
         
         self.move(self.background1View, duration: 15, translation: -528)
         self.move(self.background2View, duration: 15, translation: -528)
@@ -34,9 +49,23 @@ class ViewController: UIViewController {
         
         self.move(self.groundView, duration: 0.3, translation: -37)
         
-         self.move(self.cloud1View, duration: 23, translation: -150)
+        self.move(self.cloud1View, duration: 23, translation: -150)
         self.move(self.cloud2View, duration: 23, translation: -500)
         
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        gravityBehavior = UIGravityBehavior(items: [])
+        dynamicAnimator.addBehavior(gravityBehavior)
+        gravityBehavior.magnitude = 0.3
+        
+        collisionBehavior = UICollisionBehavior(items: [marioView])
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        collisionBehavior.addBoundary(withIdentifier: "barrier" as
+            NSCopying, for: UIBezierPath(rect: groundView.frame))
+
     }
         
     func move(_ image: UIImageView, duration: Double, translation: CGFloat) {
@@ -47,6 +76,42 @@ class ViewController: UIViewController {
                 self.move(image, duration: duration, translation: translation)
             }
         }
-
-
+    
+   /* func panGesture(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            print("Began.")
+                if marioView.frame.contains(gesture.location(in: view)) {
+                    gravityBehavior.removeItem(marioView)
+            }
+        case .changed:
+            let translation = gesture.translation(in: marioView)
+            
+            gesture.view!.center = CGPoint(x: gesture.view!.center.x + translation.x, y: gesture.view!.center.y + translation.y)
+            
+            gesture.setTranslation(CGPoint.zero, in: self.view)
+            
+            print("\(gesture.view!.center.x)=\(gesture.view!.center.y)")
+            print("t;: \(translation)")
+        case .ended:
+                if marioView.frame.contains(gesture.location(in: view)) {
+                    gravityBehavior.addItem(marioView)
+            }
+            print("Ended.")
+        case .cancelled:
+            print("Cancelled")
+        default:
+            print("Default")
+        }
+    }
+    */
+    func removeGravity() {
+        gravityBehavior.removeItem(marioView)
+        collisionBehavior.removeItem(marioView)
+    }
+    
+    func addGravity() {
+        gravityBehavior.addItem(marioView)
+        collisionBehavior.addItem(marioView)
+    }
 }
